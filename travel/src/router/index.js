38722387ccbcb8071e01bcdda46f2e389774dc2a
Hardik Patel <emailhardikpatel@gmail.com>
2020-11-02 11:21:01 -0800

    Adds routes

--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 
+import Brazil from '../views/Brazil.vue';
+import Hawaii from '../views/Hawaii.vue';
+import Jamaica from '../views/Jamaica.vue';
+import Panama from '../views/Panama.vue';
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -17,6 +22,31 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/brazil',
+    name: 'Brazil',
+    component: Brazil,
+  },
+  {
+    path: '/hawaii',
+    name: 'Hawaii',
+    component: Hawaii,
+  },
+  {
+    path: '/jamaica',
+    name: 'Jamaica',
+    component: Jamaica,
+  },
+  {
+    path: '/jamaica',
+    name: 'Jamaica',
+    component: Jamaica,
+  },
+  {
+    path: '/panama',
+    name: 'Panama',
+    component: Panama,
   },
 ];
 
